437c06c4521d451fd9c4165cc79853cd3b007618
Julian Goede <julian.goede@smartclip.tv>
2024-01-03 19:11:24 +0100

    Add dedicated examples crate
    It's much more convenient for me
    to define binaries in a dedicated examples
    section. For, now I use it personally to
    play around with what the other crates have to offer.

---
+++ examples/src/dev.rs
@@ -0,0 +1,47 @@
+// check if scheduler can register pipelines
+use coordinator::Coordinator;
+use schema::{Pipeline, Scheduler};
+
+struct StringPipeline {
+    value: String,
+}
+
+impl Pipeline<String> for StringPipeline {
+    fn new() -> Self {
+        StringPipeline {
+            value: "Hello World".to_string(),
+        }
+    }
+
+    fn run(
+        &mut self,
+        _args: &dyn std::any::Any,
+    ) -> schema::RunResult<String, Box<dyn std::error::Error>> {
+        schema::RunResult::Ok(self.value.clone())
+    }
+}
+
+struct IntegerPipeline {
+    x: u32,
+}
+
+impl Pipeline<u32> for IntegerPipeline {
+    fn new() -> Self {
+        IntegerPipeline { x: 42 }
+    }
+
+    fn run(
+        &mut self,
+        _args: &dyn std::any::Any,
+    ) -> schema::RunResult<u32, Box<dyn std::error::Error>> {
+        schema::RunResult::Ok(self.x)
+    }
+}
+
+fn main() {
+    let string_pipeline: StringPipeline = StringPipeline::new();
+    let integer_pipeline: IntegerPipeline = IntegerPipeline::new();
+    let mut coordinator = Coordinator::new();
+    coordinator.register(string_pipeline);
+    coordinator.register(integer_pipeline);
+}
